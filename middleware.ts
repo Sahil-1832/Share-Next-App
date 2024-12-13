34b49e3bed66aad86f25d0cb375dc3de62bd0b2c
Sahil-1832 <sharmasahil1832@gmail.com>
2024-12-13 16:54:07 +0530

    Updated the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs/server'
 
 export default authMiddleware({
-    publicRoutes:['/','/about-us','/contact-us']
+    publicRoutes:['/','/about-us','/contact-us','/f/']
 });
 
 export const config = {
